2d7e3ad214eb0956402041b4e66521c3b2452a87
Bilal Amiry <bilal.amiry@Bilals-Laptop.local>
2024-07-10 14:44:43 +0100

    FizzBuzz Task

---
+++ src/test/scala/Week2/Wed/FizzBuzzSpec.scala
@@ -0,0 +1,25 @@
+package Week2.Wed
+
+import org.scalatest.FlatSpec
+
+class FizzBuzzSpec extends FlatSpec {
+
+
+
+  "fizzBuzz" should "if numb is divisible by both 3 and 5, will return FizzBuzz" in {
+    val num = new FizzBuzz
+    assert(num.fizzBuzz(15) == "FizzBuzz")
+  }
+
+  "fizzBuzz" should "if num is divisible by 3 will return Fizz" in {
+    val num = new FizzBuzz()
+    assert(num.fizzBuzz(9) == "Fizz")
+  }
+
+  "fizzBuzz" should "if num is divisible by 5" in {
+    val num = new FizzBuzz
+    assert(num.fizzBuzz(10) == "Buzz")
+  }
+
+
+}
