01c78febba75f595116aa2292c389e88021c05d5
BAmiriDev <126995732+BAmiriDev@users.noreply.github.com>
2024-07-10 12:17:18 +0100

    Two numbers (#1)
    * add dependecies
    
    * complete twoNumbersSpec
    
    * add gitignore
    
    ---------
    
    Co-authored-by: Bilal Amiry <bilal.amiry@Bilals-Laptop.local>

---
+++ src/main/scala/Week2/Wed/TwoNumbers.scala
@@ -0,0 +1,27 @@
+package Week2.Wed
+
+class TwoNumbers (x: Int, y: Int) {
+
+  def add(): Int = {
+    x + y
+  }
+
+  def subtract(): Int = {
+    x - y
+  }
+
+  def multiply(): Int = {
+    x * y
+  }
+
+  def divide(): Int = {
+    x/y
+  }
+
+  //%
+  def isAMultiple(): Boolean = {
+
+    x % y == 0
+  }
+
+}
